467f32eeb92cc2c6942664450de826fa86646007
psmanningjr <psmanningjr@gmail.com>
2014-08-26 20:51:14 -0600

    convert first letter to upper case

--- soundex.cpp
+++ soundex.cpp
@@ -1,5 +1,21 @@
 #include "soundex.h"
+#include <cstring>
+
+using namespace std;
 
 Soundex::Soundex()
 {
 }
+
+string Soundex::encode(const string& word)
+{
+    return firstLetterToUpper(word);
+}
+
+string Soundex::firstLetterToUpper(const string& word)
+{
+    string newStr = word;
+    newStr[0]=toupper(word[0]);
+    return newStr;
+}
+
